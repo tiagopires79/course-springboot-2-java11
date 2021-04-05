42e1178ba36bb12f3789d337241ea82c98125946
tiagopires79 <tiagopiresdb@gmail.com>
2021-04-05 17:57:25 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -0,0 +1,32 @@
+package com.educandoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+	
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping
+	ResponseEntity<List<Order>> findAll() {
+		//List<Order> orderList = service.findAll();
+		return ResponseEntity.ok().body(service.findAll());
+	}
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Order> findById(@PathVariable Long id){
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+}
